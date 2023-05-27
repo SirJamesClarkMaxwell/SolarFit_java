de37e65a5328446d9eda29bbd5ed3a1a66778966
Franchessco <fzabier@gmail.com>
2023-05-27 14:45:55 +0200

    Dodanie ładowania pliku do programu
    Signed-off-by: Franchessco <fzabier@gmail.com>

--- src/main/java/pl/solarfit/io/FileLoader.java
+++ src/main/java/pl/solarfit/io/FileLoader.java
@@ -1,6 +1,25 @@
 package pl.solarfit.io;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileLoader
 {
+    public static List<String> readFile(String pathToFile)
+    {
+        try
+        {
+            return FileUtils.readLines(new File(pathToFile), "UTF-8");
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
 
 }

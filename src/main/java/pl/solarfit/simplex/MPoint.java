738badb5b5744fae999c2214f0a812fd38229c2d
Franchessco <fzabier@gmail.com>
2023-05-27 15:01:24 +0200

    modyfikacja MPoint o dodanie prądu powierzchniowego
    Signed-off-by: Franchessco <fzabier@gmail.com>

--- src/main/java/pl/solarfit/simplex/MPoint.java
+++ src/main/java/pl/solarfit/simplex/MPoint.java
@@ -4,23 +4,44 @@ public class MPoint
 {
     private double x;
     private double y;
+    private double z;
+
+    public MPoint(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     public MPoint()
     {
         this.x = 0.0;
         this.y = 0.0;
+        this.z = 0.0;
     }
 
     public MPoint(double tx, double ty)
     {
         this.x = tx;
         this.y = ty;
+        this.z = 0.0;
+    }
+
+    public double getZ()
+    {
+        return z;
+    }
+
+    public void setZ(double z)
+    {
+        this.z = z;
     }
 
     public void clear()
     {
         this.x = 0.0;
         this.y = 0.0;
+        this.z = 0.0;
     }
 
     public void setXY(double tx, double ty)

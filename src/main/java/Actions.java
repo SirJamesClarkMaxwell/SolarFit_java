331e2eb4d5950807aefb1369025c99ca218a2d7c
Franchessco <fzabier@gmail.com>
2023-05-27 12:03:03 +0200

    1) dodanie trzech z czterech paneli w oknie actions
    Signed-off-by: Franchessco <fzabier@gmail.com>

--- src/main/java/Actions.java
+++ src/main/java/Actions.java
@@ -1,3 +1,46 @@
+import javax.swing.*;
+
 public class Actions
 {
+    private JPanel parameters;
+    private JPanel simplexsettings;
+    private JPanel inputdata;
+    private JPanel montecarlosettings;
+    private JTextField I0textfield;
+    private JLabel I0label;
+    private JTextField Atextfield;
+    private JTextField Rstextfield;
+    private JTextField Rchtextfiield;
+    private JLabel Rchlabel;
+    private JLabel Rslabel;
+    private JLabel Alable;
+    private JLabel Fittedparameterslabel;
+    private JPanel Asimplexparameterslabel;
+    private JLabel simplexparameterslable;
+    private JLabel I0simplexsettingslabel;
+    private JLabel minlimitsimplexlable;
+    private JLabel Asimplexsettingslabel;
+    private JLabel Rssimplexsettingslabel;
+    private JLabel Rchsimplexsettingslabel;
+    private JTextField I0minlimitsimplex;
+    private JTextField Aminlimitsimplex;
+    private JTextField Rsminlimitsimplex;
+    private JTextField Rchminlimitsimplex;
+    private JTextField I0maxlimitsimplex;
+    private JTextField Amaxlimitsimplex;
+    private JTextField Rsmaxlimitsimplex;
+    private JTextField Rchmaxlimitsimplex;
+    private JTextField noisemcsettings;
+    private JTextField iterationmcsettings;
+    private JLabel noisemcsettingslabel;
+    private JLabel iterationmcsettingslablel;
+    private JPanel actions;
+    private JRadioButton twocoluminputdataradiobutton;
+    private JRadioButton threecolumninputdataradiobutton;
+    private JLabel inputdatalabel;
+
+    private void createUIComponents()
+    {
+        // TODO: place custom component creation code here
+    }
 }

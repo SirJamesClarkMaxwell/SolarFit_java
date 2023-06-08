53e026d336dc76c60cadd86fa98ca47547f46e02
Franchessco <fzabier@gmail.com>
2023-06-08 22:15:39 +0200

    commit przejściowy

--- src/main/java/pl/solarfit/io/WriteData.java
+++ src/main/java/pl/solarfit/io/WriteData.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class WriteData
 {
-    public void savaDataIntoFile
+    public void savaDataIntoFile;
     private  String nameOfFile;
     private String path;
     private List<String> toWrite;

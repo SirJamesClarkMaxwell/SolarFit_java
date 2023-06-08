e18a15d9723acace81fd65b844a116d3daf93830
Franchessco <fzabier@gmail.com>
2023-06-08 21:06:34 +0200

    1) deklaracja prefit i Fit 2) napisanie klasy FitDataStorage która przechowuje historię simplexu 3) w klasie FitDataStorage zdefiniowano metodę toList która zwraca liste stringów gotową do zapisania do pliku 4) dodanie klasy Parameters, którą agreguje klasa FitDataStorage i wewnątrz niej są przechowywany punkt simplexu, ma metodę toSting: pozwala ona na konwersje zawartości obiektu do stringu aby móc zapisać wygodnie do pliku
    1) do napisania w pakege io klasę która bedzie zrzucała informacje do pliku
    2) napisanie fit i prefit

---
+++ src/main/java/pl/solarfit/fit/Parameters.java
@@ -0,0 +1,68 @@
+package pl.solarfit.fit;
+
+public class Parameters
+{
+    private double A;
+    private double I0;
+    private double Rs;
+    private double Rch;
+
+    public Parameters(double a, double i0, double rs, double rch)
+    {
+        A = a;
+        I0 = i0;
+        Rs = rs;
+        Rch = rch;
+    }
+
+    public double getA()
+    {
+        return A;
+    }
+
+    public void setA(double a)
+    {
+        A = a;
+    }
+
+    public double getI0()
+    {
+        return I0;
+    }
+
+    public void setI0(double i0)
+    {
+        I0 = i0;
+    }
+
+    public double getRs()
+    {
+        return Rs;
+    }
+
+    public void setRs(double rs)
+    {
+        Rs = rs;
+    }
+
+    public double getRch()
+    {
+        return Rch;
+    }
+
+    public void setRch(double rch)
+    {
+        Rch = rch;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder toString = new StringBuilder();
+        toString1.append("A  =" + this.A);
+        toString1.append("I0  =" + this.I0);
+        toString1.append("Rs  =" + this.Rs);
+        toString1.append("Rch  =" + this.Rch);
+        return toString;
+    }
+}

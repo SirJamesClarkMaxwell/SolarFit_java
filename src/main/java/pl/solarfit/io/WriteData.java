579342dfe3189c02fe2dba8982477b5fecaec669
Franchessco <fzabier@gmail.com>
2023-06-08 21:13:24 +0200

    1) dodanie klasy która zapisuje przebieg simplexu do pliku po podaniu jego ścieżni i nazwy

---
+++ src/main/java/pl/solarfit/io/WriteData.java
@@ -0,0 +1,32 @@
+package pl.solarfit.io;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class WriteData
+{
+    public void savaDataIntoFile
+    private String nameOfFile;
+    private String path;
+    private List<String> toWrite;
+
+
+    public WriteData(String nameOfFile, String path, List<String> toWrite)
+    {
+        this.nameOfFile = nameOfFile;
+        this.path = path;
+        this.toWrite = toWrite;
+    }
+
+    public void saveDataIntoFile()
+    {
+        Path output = Paths.get(this.path);
+        try
+        {
+            Files.write(output, this.toWrite);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
